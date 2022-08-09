4e0825891a9305548f9aa28d011b2ef6267a0c81
xumao <44562741+sentibeitaokong@users.noreply.github.com>
2022-08-09 15:55:28 +0800

    call/apply/bind update

--- NativeJs/Function.js
+++ NativeJs/Function.js
@@ -1,8 +1,19 @@
+// ES3
 //call()方法
+//保证fn属性的唯一性
+function fnFactory(context) {
+    var unique_fn = "fn";
+    while (context.hasOwnProperty(unique_fn)) {
+        unique_fn = "fn" + Math.random(); // 循环判断并重新赋值
+    }
+
+    return unique_fn;
+}
 Function.prototype.Mycall=function (context) {
     //如果context是undefined或者Null就赋值为window,否则转换为对象
     var context=context == undefined ? window : Object(context);
-    context.fn=this
+    var fn = fnFactory(context); // added
+    context[fn]=this
     var args=[]
     for(let i=1,len=arguments.length;i<len;i++){
         args.push(`arguments[${i}]`);
@@ -11,12 +22,25 @@ Function.prototype.Mycall=function (context) {
     delete context.fn
     return result
 }
+//ES6
+/*Function.prototype.Mycall = function (context) {
+    context = context ? Object(context) : window;
+    var fn = Symbol(); // added
+    context[fn] = this; // changed
+    let args = [...arguments].slice(1);
+    let result = context.fn(...args);
 
+    delete context.fn
+    return result;
+}*/
+
+//ES3
 // apply方法
 Function.prototype.MyApply=function (context,arr) {
     //如果context是undefined或者Null就赋值为window,否则转换为对象
     var context=context == undefined ? window : Object(context);
-    context.fn=this
+    var fn = fnFactory(context); // added
+    context[fn]=this
     var result
     // 当arr传参不是对象或者方法时，就报错
     if(typeof arr!=='object'&&typeof arr!=='function'&&typeof  arr!=='undefined'){
@@ -35,8 +59,28 @@ Function.prototype.MyApply=function (context,arr) {
     delete context.fn
     return result
 }
+
+//ES6
+/*Function.prototype.MyApply = function (context, arr) {
+    context = context ? Object(context) : window;
+    var fn = Symbol(); // added
+    context[fn] = this; // changed
+    let result;
+    if (!arr) {
+        result = context.fn();
+    } else {
+        result = context.fn(...arr);
+    }
+
+    delete context.fn
+    return result;
+}*/
+
 // bind方法
 Function.prototype.MyBind=function (context) {
+    if (typeof this !== "function") {
+        throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
+    }
     var self=this
     var args=Array.prototype.slice.call(arguments,1)
     var fNOP=function () {
